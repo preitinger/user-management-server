bada04ff319afe19cfeeba05d8869ea129ec733d
preitinger <peter.reitinger@gmail.com>
2024-01-10 11:58:43 +0100

    POST function not in userManagementServer.ts

--- userManagementServer.ts
+++ userManagementServer.ts
@@ -3,13 +3,9 @@ import { myPOST } from "./apiRoutesForServer";
 import { RegisterReq, RegisterResp } from "./user-management-common/register";
 import { ApiResp } from "./user-management-common/apiRoutesCommon";
 
-const executeRegister = async (req: RegisterReq): Promise<ApiResp<RegisterResp>> => {
+export const executeRegister = async (req: RegisterReq): Promise<ApiResp<RegisterResp>> => {
     const resp: RegisterResp = {
         type: 'success'
     }
     return resp;
-}
-
-export async function POST(req: NextRequest) {
-    return myPOST<RegisterReq, RegisterResp>(req, executeRegister);
 }
